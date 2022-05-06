f8e79cfda75d766414cb53146a42bdf4b3074051
nickspeedy0@gmail.com <nickspeedy0@gmail.com>
2022-05-06 17:15:47 +0930

    removed bits from pa, pb

--- src/sorty.c
+++ src/sorty.c
@@ -7,13 +7,13 @@ void rot_min(t_data *data)
         find_max(data);
         if (data->stacka->content == data->min)
             pb(data, 1);
-        else if (node_index(data, data->min) > ft_lsttsize(data->stacka))
+        else if (node_index(data, data->min) > ft_lsttsize(data->stacka) / 2)
             rra(data, 1);
         else if (node_index(data, data->min) < ft_lsttsize(data->stacka))
             ra(data, 1);
     }
     sort5d(data);
-    while (ft_lsttsize(data->stacka) < 100)
+    while (ft_lsttsize(data->stackb) > 0)
         pa(data, 1);
     printf("Operations used = %d\n", data->operations);
     
